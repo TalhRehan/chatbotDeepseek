07d0037d7083bfb25f7f21b2356d21edde03b93b
TalhRehan <ch.talha223@gmail.com>
2025-03-10 18:00:39 +0500

    code update

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, jsonify, render_template
 from langchain_ollama.llms import OllamaLLM
 from langgraph.graph import StateGraph, START, END
 from typing import List, Dict
- # Import regex for removing tags
+import re # Import regex for removing tags
 
 app = Flask(__name__, template_folder="")
 
